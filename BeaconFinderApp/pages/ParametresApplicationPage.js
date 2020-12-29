81bb8fe3f4583bf9a140f9f9d8933b11d6a22e75
Nicooow <nicolas.boujot@icloud.com>
2020-12-29 17:08:13 +0100

    ParametresApplication changement en class

--- BeaconFinderApp/pages/ParametresApplicationPage.js
+++ BeaconFinderApp/pages/ParametresApplicationPage.js
@@ -1,80 +1,97 @@
 import * as React from 'react';
+import {Component, useRef} from 'react';
 import { SafeAreaView, StyleSheet, View, Text, Image } from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 import { useTheme } from '@react-navigation/native';
 import SettingsList from 'react-native-settings-list';
 
 import Dialog from "react-native-dialog";
 
-const ParametresApplication = () => {
+export class ParametresApplicationPageWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameModalVisible: false,
+      visibleOnMap: false,
+      name: "Defaut",
+      newName: ""
+    };
+    this.textInput = React.createRef();
+  }
 
-  const { colors } = useTheme();
-  const [nameModalVisible, setNameModalVisible] = React.useState(false);
-  const [visibleOnMap, setVisibleOnMap] = React.useState(false);
+  render() {
+    const { theme, navigation } = this.props;
+    colors = theme.colors
 
-  const styles = StyleSheet.create({
-    titleInfoStyle: {
-      color:'#aaa'
-    },
-    titleStyle: {
-      color:colors.text
-    }
-  });
+    const styles = StyleSheet.create({
+      titleInfoStyle: {
+        color:'#aaa'
+      },
+      titleStyle: {
+        color:colors.text
+      }
+    });
 
-  return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ flex: 1 }}>
-          <View style={{flex:1}}>
-              <SettingsList borderColor={colors.border} backgroundColor={colors.card} defaultTitleStyle={styles.titleStyle} defaultItemSize={50}>
-                <SettingsList.Header headerStyle={{marginTop:15}}/>
-                <SettingsList.Item
-                  hasSwitch={true}
-                  switchState={visibleOnMap}
-                  hasNavArrow={false}
-                  title='Partager sa position'
-                  switchOnValueChange={(value) => {setVisibleOnMap(value);}}
-                />
-                <SettingsList.Item
-                  title='Nom sur la carte'
-                  titleInfo='Nicolas'
-                  titleInfoStyle={styles.titleInfoStyle}
-                  onPress={() => setNameModalVisible(true)}
-                />
-                <SettingsList.Header headerStyle={{marginTop:15}}/>
-                <SettingsList.Item
+      return (
+        <>
+          <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+              <View style={{flex:1}}>
+                  <SettingsList borderColor={colors.border} backgroundColor={colors.card} defaultTitleStyle={styles.titleStyle} defaultItemSize={50}>
+                    <SettingsList.Header headerStyle={{marginTop:15}}/>
+                    <SettingsList.Item
+                      hasSwitch={true}
+                      switchState={this.state.visibleOnMap}
+                      hasNavArrow={false}
+                      title='Partager sa position'
+                      switchOnValueChange={(value) => this.setState({ visibleOnMap: value })}
+                    />
+                    <SettingsList.Item
+                      title='Nom sur la carte'
+                      titleInfo={this.state.name}
+                      titleInfoStyle={styles.titleInfoStyle}
+                      onPress={() => this.setState({ nameModalVisible: true })}
+                    />
+                    <SettingsList.Header headerStyle={{marginTop:15}}/>
+                    <SettingsList.Item
 
-                  title='aaa'
-                  onPress={() => Alert.alert('Route To Notifications Page')}
-                />
-                <SettingsList.Item
+                      title='aaa'
+                      onPress={() => Alert.alert('Route To Notifications Page')}
+                    />
+                    <SettingsList.Item
 
-                  title='aaa'
-                  onPress={() => Alert.alert('Route To Control Center Page')}
-                />
-                <SettingsList.Item
+                      title='aaa'
+                      onPress={() => Alert.alert('Route To Control Center Page')}
+                    />
+                    <SettingsList.Item
 
-                  title='aaa'
-                  onPress={() => Alert.alert('Route To Do Not Disturb Page')}
-                />
-              </SettingsList>
+                      title='aaa'
+                      onPress={() => Alert.alert('Route To Do Not Disturb Page')}
+                    />
+                  </SettingsList>
+                </View>
             </View>
-        </View>
-      </SafeAreaView>
+          </SafeAreaView>
 
-      <View>
-        <Dialog.Container visible={nameModalVisible} onBackdropPress={() => setNameModalVisible(false)}>
-          <Dialog.Title>Nom sur la carte</Dialog.Title>
-          <Dialog.Description>
-            Choisissez le nom qui sera affiché sur la carte pour les autres utilisateurs de l'application
-          </Dialog.Description>
-          <Dialog.Input />
-          <Dialog.Button label="Annuler" onPress={() => setNameModalVisible(false)}/>
-          <Dialog.Button label="Confirmer" onPress={() => setNameModalVisible(false)}/>
-        </Dialog.Container>
-      </View>
-    </>
-  );
+          <View>
+            <Dialog.Container visible={this.state.nameModalVisible} onBackdropPress={() => this.setState({ nameModalVisible: false })}>
+              <Dialog.Title>Nom sur la carte</Dialog.Title>
+              <Dialog.Description>
+                Choisissez le nom qui sera affiché sur la carte pour les autres utilisateurs de l'application
+              </Dialog.Description>
+              <Dialog.Input onChangeText={(text) => this.setState({ newName: text })} value={this.state.name} />
+              <Dialog.Button label="Annuler" onPress={() => this.setState({ nameModalVisible: false })}/>
+              <Dialog.Button label="Confirmer" onPress={() => console.log(this.textInput)}/>
+            </Dialog.Container>
+          </View>
+        </>
+      );
+    }
 };
 
-export default ParametresApplication;
+export default function ParametresApplicationPage(props, prop) {
+  const theme = useTheme();
+
+  return <ParametresApplicationPageWrapper {...props} theme={theme} />;
+}
